6172af187af9f3dd43f990c53347a256abde5834
Arjun01 <erarjun0@gmail.com>
2020-08-25 11:56:01 +0530

    Button states for send location button

--- public/js/chat.js
+++ public/js/chat.js
@@ -4,6 +4,7 @@ const socket = io()
 const $messageForm = document.querySelector('#message-form');
 const $messageFormInput = $messageForm.querySelector('input');
 const $messageFormButton = $messageForm.querySelector('button');
+const $sendLocationButton = document.querySelector('#send-location');
 
 socket.on('message',(message)=>{
     console.log(message);
@@ -27,17 +28,21 @@ $messageForm.addEventListener('submit',(e)=>{
     });
 })
 
-document.querySelector('#send-location').addEventListener('click', () =>{
+$sendLocationButton.addEventListener('click', () =>{
     if(!navigator.geolocation){
         return alert('Geolocation is not supported by your browser.');
     }
     
+    $sendLocationButton.setAttribute('disabled', 'disabled');
+
     navigator.geolocation.getCurrentPosition( (position) =>{
         
         socket.emit('sendLocation',{
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         }, ()=>{
+         
+            $sendLocationButton.removeAttribute('disabled');
                 console.log('Location Shared!');
         });
     })
