9dcd84c08e339bcea41364d320829aab7bdff95e
Arjun01 <erarjun0@gmail.com>
2020-08-25 11:33:35 +0530

    filtering bad-words over chat

--- public/js/chat.js
+++ public/js/chat.js
@@ -9,8 +9,11 @@ document.querySelector('#message-form').addEventListener('submit',(e)=>{
 
     const message = e.target.elements.message.value;
 
-    socket.emit('sendMessage', message, (message)=>{
-        console.log('The message was delivered!', message)
+    socket.emit('sendMessage', message, (error)=>{
+        if(error){
+            return console.log(error);
+        }
+        console.log('The message was delivered!')
     });
 })
 
